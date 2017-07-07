6d90bb30656b074d86acd94296bb069b825c6d8a
hugosvent <ventzlienata@gmail.com>
2017-07-07 19:13:13 +0700

    fix resize width multiple image

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -175,6 +175,16 @@ abstract class Controller extends BaseController
                         array_push($filesPath, $path.$filename.'.'.$file->getClientOriginalExtension());
                         $filePath = $path.$filename.'.'.$file->getClientOriginalExtension();
 
+                        $options = json_decode($row->details);
+
+                        if (isset($options->resize) && isset($options->resize->width) && isset($options->resize->height)) {
+                            $resize_width = $options->resize->width;
+                            $resize_height = $options->resize->height;
+                        } else {
+                            $resize_width = 1800;
+                            $resize_height = null;
+                        }
+
                         $image = Image::make($file)->resize($resize_width, $resize_height,
                             function (Constraint $constraint) {
                                 $constraint->aspectRatio();

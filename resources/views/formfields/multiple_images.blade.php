35cdc94f3b17d2e9b193959ef24ace190c6c0b9e
hugosvent <ventzlienata@gmail.com>
2017-07-12 19:10:07 +0700

    delete multiple image

--- resources/views/formfields/multiple_images.blade.php
+++ resources/views/formfields/multiple_images.blade.php
@@ -2,12 +2,15 @@
 @if(isset($dataTypeContent->{$row->field}))
     <?php $images = json_decode($dataTypeContent->{$row->field}); ?>
     @if($images != null)
-        @foreach($images as $image)
-            <div class="img_settings_container" data-field-name="{{ $row->field }}">
-                <img src="{{ Voyager::image( $image ) }}" data-image="{{ $image }}" data-id="{{ $dataTypeContent->id }}">
-                <a href="#" class="voyager-x remove-multi-image"></a>
-            </div>
-        @endforeach
+      @foreach($images as $image)
+        <div class="img_settings_container" data-field-name="{{ $row->field }}">
+          <img src="{{ Voyager::image( $image ) }}" data-image="{{ $image }}" data-id="{{ $dataTypeContent->id }}">
+          <a href="#" class="voyager-x remove-multi-image"></a>
+        </div>
+        <div class="image-tools">
+          <i class="glyphicon glyphicon-remove remove-multi-image" title="Remove image"></i>
+        </div>
+      @endforeach
     @endif
 @endif
 <div class="clearfix"></div>
